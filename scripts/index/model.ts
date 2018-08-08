49a488308f7567e96a5a13b32da5533999f38777
rodrigo debian <correo.rodrigo@gmail.com>
2018-08-08 09:44:25 -0500

    merging bootstrap setup

--- scripts/index/model.ts
+++ scripts/index/model.ts
@@ -2,14 +2,14 @@ import * as ko from "knockout";
 import { FormBase, IField } from "../valiko";
 
 class Model extends FormBase {
-    public firstname: KnockoutObservable<string>;
+    public firstname: IField<string>;
     public lastname: IField<string>;
 
     constructor(firstname: string, lastname: string) {
         super();
         
-        this.firstname = ko.observable<string>(firstname);
-        this.lastname = this.addField([], lastname);
+        this.firstname = this.addField<string>([], firstname);
+        this.lastname = this.addField<string>([], lastname);
         
     }
 }
